98a911d9ce6b7b06068303dfc6322efe796fd262
jkwang <97539273+jiakaiss@users.noreply.github.com>
2023-06-26 10:12:05 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -17,15 +17,15 @@ if __name__ == '__main__':
         print('--------------------开始检测-----------------------')
         try:
             # 访问的应用接口
-            is_free = requests.get('http://127.0.0.1:8000/is_free')
-            if is_free.text == '1':
-                # 若访问某个接口的状态码不正确，运行另一个脚本以杀死某一程序
-                response = requests.post('http://127.0.0.1:8000/', json={"message": "你好"},
-                                         headers={'Content-Type': 'application/json'})
-                if response.status_code != 200:
-                    os.system('python kill_process.py')
-                    print('--------------------重新运行程序-----------------------')
-                    os.system("gnome-terminal -e 'bash -c \"python {}\"'".format(args.path))
+            # 若访问某个接口的状态码不正确，运行另一个脚本以杀死某一程序
+            if (requests.get('http://127.0.0.1:8000/is_free').text != '1' or
+                    requests.post('http://127.0.0.1:8000/', json={"message": "你好"},
+                                  headers={'Content-Type': 'application/json'}).status_code != 200):
+                os.system('python kill_process.py')
+                print('--------------------重新运行程序-----------------------')
+                os.system("gnome-terminal -e 'bash -c \"python {}\"'".format(args.path))
+            else:
+                print('--------------------程序运行正常-----------------------')
         except requests.exceptions.ConnectionError:
             print('--------------------该程序未运行，开始运行该程序-----------------------')
             os.system("gnome-terminal -e 'bash -c \"python {}\"'".format(args.path))
